3b724504584cb45f35c60e163df9b2ab52701794
magicpuddingcat <danielle_rebecca_9@hotmail.com>
2020-02-11 14:32:00 +1100

    Calculator now able to generate pay amounts and display to GUI.

--- Calc.py
+++ Calc.py
@@ -1,23 +1,24 @@
 """Simple pay calculator with ability to calculate total pay before
-    tax for given shifts, including weekends and public holidays. Work commenced
+    tax for my current job, including weekends and public holidays. Work commenced
     17/12/2019, Danielle"""
 
 from tkinter import *
 from math import *
 
-"""Create the calculator, which can gather employment data from the user
-    and then generate a pre-tax pay sum."""
 class Calc:
 
-    rate = 36.6
+    rate = 48.0
     day_allowance = 29.7
-    night_allowance = 80.3
+    night_allowance = 82.0
     weekend_loading = 0.5
-    holiday_loading = 1
+    holiday_loading = 1.0
 
     def __init__(self, master):
         self.master = master
         master.title("My Pay Calculator")
+
+        self.title = Label(master, text="Calculate my pay!")
+        self.title.pack()
 
         # add all of the labels to prompt the user for input and the input fields
         #
@@ -52,19 +53,34 @@ class Calc:
         self.user_holiday_hours.pack()
 
         # add button that will return the weekly payment sum to the user
-        self.pay_calc_button = Button(master, text = "Calculate Pay", command = self.say_hello())
+        self.pay_calc_button = Button(master, text="Calculate Pay", command=self.display)
         self.pay_calc_button.pack()
 
-    # Note, THIS IS TO BE DELETED WHEN A MORE SUITABLE FUNCTION IS READY
-    def say_hello(self):
-        print("Hello!")
+        #add the lable that will display the final amount
+        self.pay_display = Label(self.master, text="")
+        self.pay_display.pack()
 
-    def calculate_pay(self, total_hours, nights, days, hours_wkend, hours_holiday):
-        pay = total_hours * self.rate + nights * self.night_allowance + \
-            days * self.day_allowance + hours_wkend * self.rate * self.weekend_loading \
-            + hours_holiday * self.rate * self.holiday_loading
+    def calculate_pay(self):
+        # If the user has entered something in all fields and clicks calculate, calculate pay
+        pay = self.num_total_hours * self.rate + self.num_nights * self.night_allowance + \
+              self.num_days * self.day_allowance + \
+            self.num_hours_wkend * self.rate * self.weekend_loading \
+              + self.num_hours_holiday * self.rate * self.holiday_loading
         return pay
 
+    def display(self):
+
+        # Get all relevant values entered by the user
+        self.num_total_hours = float(self.user_total_hours.get())
+        self.num_days = float(self.user_days.get())
+        self.num_nights = float(self.user_nights.get())
+        self.num_hours_wkend = float(self.user_wkend_hours.get())
+        self.num_hours_holiday = float(self.user_holiday_hours.get())
+
+        if (float(self.num_total_hours + self.num_hours_wkend + self.num_hours_holiday))>=0:
+            self.pay_amount = self.calculate_pay()
+            self.pay_display['text'] = str(self.pay_amount)
+            self.pay_display.pack()
 
 # Initialize the GUI window
 root = Tk()
